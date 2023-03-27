dd8fe74091820a4689f2c1289661ddf5dcbf1e26
Victor Chaves Moreira <vchavesmoreira@gmail.com>
2023-03-27 14:58:19 -0300

    Update variaveis.py

--- demanda/variaveis.py
+++ demanda/variaveis.py
@@ -3,3 +3,7 @@ b=int(input("Digite um número inteiro: "))
 soma=a+b
 print(soma)
 print(f"A soma {a}+{b} é igual a {a+b}")
+if soma == a+b:
+    print(f"A soma está correta.")
+else:
+    print(f"A soma está incorreta.")
