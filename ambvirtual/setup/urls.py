97640635d78f8ee00ede426575c2f0e2b8145a8a
Victor Moreira <vchavesmoreira@gmail.com>
2023-03-29 17:48:12 -0300

    atividade dia 3 finalizada

--- ambvirtual/setup/urls.py
+++ ambvirtual/setup/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from videogame_store_app import JogoViewSet, LojaViewSet
+
+router = routers.DefaultRout()
+router.register('jogo', JogoViewSet, basename='Jogo')
+router.register('loja', LojaViewSet, basename='Loja')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
--- ambvirtual/videogame_store_app/views.py
+++ ambvirtual/videogame_store_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Jogo
-from serializer import JogoSerializer
+from serializer import JogoSerializer, LojaSerializer
 from .models import Loja
+from rest_framework import viewsets
+
 # Create your views here.
-class JogoViewSet(views.ModelViewSet):
+class JogoViewSet(viewsets.ModelViewSet):
     queryset = Jogo.objects.all()
     serializer_class = JogoSerializer
-class LojaViewSet(views.ModelViewSet):
+    
+class LojaViewSet(viewsets.ModelViewSet):
     queryset = Loja.objects.all()
     serializer_class = LojaSerializer
